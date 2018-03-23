7e4f60ba9d6be724591a1c6cf470544aad38a8c3
alaadindev <alaadindev@gmail.com>
2018-03-23 13:39:51 +0200

    update 1

---
+++ new/webproject/index.php
@@ -0,0 +1,13 @@
+<?php
+ header('Location: /project/test.php');
+?>
+<html>
+<head>
+
+</head>
+<body>
+  hello world
+</form>
+</body>
+
+</html>
---
+++ new/webproject/project/control/createuser.php
@@ -0,0 +1,30 @@
+<?php
+required 'main.php';
+if ($_SERVER['REQUEST_METHOD']==='POST'){
+  if(isset($_POST['username'])
+    &&isset($_POST['password'])
+    &&isset($_POST['phone'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $phone = $_POST['phone'];
+      if(check::checkuser($username, $password, $phone)){
+        $stmt = $conn->prepare("INSERT INTO user (username, password, phonenumber) value (?,?,?)");
+        $stmt->bind_param('sss',$username, $password, $phone);
+        $stmt->execute();
+        $stmt->close();
+        $stmt->close();
+
+
+
+      }else{
+
+      }
+
+    }else{
+
+    }
+}else{
+
+}
+
+?>
